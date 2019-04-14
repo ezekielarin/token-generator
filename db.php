4dd4ae74461c4f07edbe1607303830bf9887603f
ezekiel shapiro arin <ezekielarin@gmail.com>
2019-04-14 10:44:54 +0100

    Create db.php

---
+++ db.php
@@ -0,0 +1,10 @@
+
+<?php
+$host = "localhost";
+$username = "root";
+$password = "11dbpass";
+$database = "token-generator";
+$con = mysqli_connect($host,$username,$password);
+mysqli_select_db($con,$database);
+
+?>
